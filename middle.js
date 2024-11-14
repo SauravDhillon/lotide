6585a4c7324969cd54f522ce13d0e950c59a7362
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-13 22:18:20 -0500

    Updates were made to the middle.js file

--- middle.js
+++ middle.js
@@ -44,5 +44,5 @@ const eqArrays = function(arr1, arr2){
   console.log(middle([1, 2, 3]));
   console.log(middle([1, 2, 3, 4, 5]));
   console.log(middle([1,2,3,4,5,6,7,8]));
-console.log(assertArraysEqual(middle([1,2,3,4]), [1,2]));
-console.log(assertArraysEqual(middle([]), [1,2]));
+assertArraysEqual(middle([1,2,3,4]), [1,2]);
+assertArraysEqual(middle([]), [1,2]);
