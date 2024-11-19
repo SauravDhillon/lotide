d438a5e8322034f2b92a83494250e12587f6a425
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-19 12:04:10 -0500

    New file created to keep tests of assertEqual.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+//TEST CODE
+const assertEqual = require("../assertEqual");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Labs", "Labs");
+assertEqual(2, 2);
+assertEqual(1, 3);
