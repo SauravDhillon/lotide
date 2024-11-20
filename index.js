9d2e14248e25e818115511122f14ea4ea9ef6834
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-19 20:02:03 -0500

    New files created for object to hold all lotide functions

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
+
 module.exports = {
   head: head,
   tail: tail,
@@ -28,7 +28,6 @@ module.exports = {
   findKey: findKey,
   findKeyByValue: findKeyByValue,
   letterPositions: letterPositions,
-  map: map,
   takeUntil: takeUntil,
   without: without
 };
